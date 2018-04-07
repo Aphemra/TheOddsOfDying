b80af1dcf2dbf38f006a6499a549dca3e9b55079
Nic <aphemra@gmail.com>
2018-04-07 03:44:29 -0400

    Menu Scene Updated
    - Added game logo
    - Added custom sprite changes for unpressed, hover, and click actions for menu buttons
    - Cleaned up code and modularized it to a MenuManager empty GameObject.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,33 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-    //Takes current scene index and adds 1 to it.
-    //Since this is for the main menu, the main menu
-    //has an index of 0, and the first scene of the
-    //game will have an index of 1
-	public void PlayGame ()
+    public GameObject mainMenuCanvas;
+    public GameObject helpMenuCanvas;
+
+    // Loads next scene (should be first level)
+	public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    //Function to quit the application from main menu
-    public void QuitGame ()
+    // Switches between MainMenu and HelpMenu
+    public void SwitchMenu_MainHelp()
     {
-        //application won't close in editor
-        //this code lets us know function works
-        Debug.Log("Quit");
-
-        Application.Quit();//closes application
+        mainMenuCanvas.SetActive(!mainMenuCanvas.activeSelf);
+        helpMenuCanvas.SetActive(!helpMenuCanvas.activeSelf);
     }
 
-
-    //just some code to test if clicking the button works
-    public void ClickTest ()
+    // Quits game
+    public void Exit()
     {
-        Debug.Log("clicked");
+        Debug.Log("Quit"); // For in editor only
+
+        Application.Quit(); // Closes application
     }
 }
